8d8b80d1d0f3a2845706f32eeb8674a5676300c0
RCurvo <renan.curvo@gmail.com>
2022-09-13 18:18:03 -0300

    implements mobile Home template

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,32 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+import { StatusBar } from 'react-native';
+import {useFonts,
+Inter_400Regular,
+Inter_600SemiBold,
+Inter_700Bold,
+Inter_900Black
+} from '@expo-google-fonts/inter'
+import { Background } from './src/components/Background';
+import {Home} from './src/screens/Home'
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+ const [fontsLoaded] = useFonts({
+Inter_400Regular,
+Inter_600SemiBold,
+Inter_700Bold,
+Inter_900Black
+  })
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <Button title="send 1"/>
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+    <StatusBar
+    barStyle='light-content'
+    backgroundColor='transparent'
+    translucent
+    />
+
+    {fontsLoaded? <Home /> : <Loading/>}
+    </Background>
   );
 }
-
-interface ButtonProps{
-  title: String;
-}
-
-function Button(props:ButtonProps){
-  return(
-  <TouchableOpacity>
-    <Text>{props.title}</Text>
-  </TouchableOpacity>)
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,31 @@
+import { View, Image, FlatList } from 'react-native';
+import LogoImg from '../../assets/logo-nlw-esports.png'
+import { GameCard } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
+import { GAMES } from '../../utils/games';
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+<Image source={LogoImg}
+style={styles.logo}
+/>
+<Heading title="Econtre o seu duo!" subtitle="Selecione o game que deseja jogar..."/> 
+
+<FlatList 
+contentContainerStyle={styles.contentList}
+data={GAMES}
+keyExtractor={item => item.id}
+renderItem={({item}) =>(
+  <GameCard 
+data={item}
+ />
+  )}
+  showsHorizontalScrollIndicator={false}
+  horizontal
+
+/>
+   </View>
+  );
+}
