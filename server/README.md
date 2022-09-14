92008583a41334465f8dc5e9d2117e0eb78f83e6
RCurvo <renan.curvo@gmail.com>
2022-09-14 10:20:11 -0300

    Implements basic routes and instaled prisma

---
+++ server/README.md
@@ -0,0 +1,30 @@
+## back-end
+
+## Entidades
+
+### Game
+
+id
+title
+bannerUrl
+
+### Ad
+
+id
+gameId
+name
+yearsPlaying
+discord
+weekDays
+hourStart
+hourEnd
+useVoiceChannel
+createdAt
+
+## Caso de Uso
+
+- Listagem de games com contagem de anúncios
+- Criação de novo anúncio
+- Listagem de anúncios por game
+- Buscar discord pelo ID do anúncio
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,31 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express()
 
 const PORT = 3333;
 
-app.get("/ads", (request, response)=> {
-return response.json([
-    {id: 1, name: "dota",},
-    {id: 2, name: "lol",},
-    {id: 3, name: "hon",},
-    {id: 4, name: "HotS",}
+app.get('/games', (request, response) => {
+    return response.json([]);
+})
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+})
+
+app.get("/games/:id/ads", (request, response)=> {
+    const gameId = request.params.id  
+    return response.json([
+        {id: 1, name: "dota",},
+        {id: 2, name: "lol",},
+        {id: 3, name: "hon",},
+        {id: 4, name: "HotS",}
+])
+})
+
+app.get("/ads/:id/discord", (request, response)=> {
+    const adId = request.params.id  
+    return response.json([
+
 ])
 })
 
