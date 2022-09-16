11b02eb6918c2dcbb203f5939a99094442178eee
RCurvo <renan.curvo@gmail.com>
2022-09-16 18:14:39 -0300

    Imports data from backend

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,33 +1,35 @@
-import { TouchableOpacity,TouchableOpacityProps, ImageBackground, ImageSourcePropType, Text } from 'react-native';
+import { TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType, Text } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { styles } from './styles';
 import { THEME } from '../../theme';
 
 export interface GameCardProps {
-    id: string;
-    name: string;
-    ads: string;
-    cover: ImageSourcePropType;
+  id: string;
+  title: string;
+  _count: {
+    ads: number;
+  }
+  bannerUrl: string;
 }
 
-interface Props extends TouchableOpacityProps{
-    data: GameCardProps
+interface Props extends TouchableOpacityProps {
+  data: GameCardProps
 }
 
-export function GameCard({data, ...rest}: Props) {
+export function GameCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-        <ImageBackground 
+      <ImageBackground
         style={styles.cover}
-        source={data.cover}
-        >
+        source={{ uri: data.bannerUrl }}
+      >
         <LinearGradient
-        colors={THEME.COLORS.FOOTER}
-        style={styles.footer}>
-          <Text style={styles.name}>{data.name}</Text>
-          <Text style={styles.ads}>{data.ads} anúncios</Text>
+          colors={THEME.COLORS.FOOTER}
+          style={styles.footer}>
+          <Text style={styles.name}>{data.title}</Text>
+          <Text style={styles.ads}>{data._count.ads} anúncios</Text>
         </LinearGradient>
-        </ImageBackground>
-    </TouchableOpacity>
+      </ImageBackground>
+    </TouchableOpacity >
   );
 }
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,31 +1,40 @@
+import { useEffect, useState } from 'react';
 import { View, Image, FlatList } from 'react-native';
 import LogoImg from '../../assets/logo-nlw-esports.png'
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
 import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+
+  useEffect(() => {
+    fetch("http://192.168.15.10:3333/games")
+      .then(response => response.json())
+      .then(data => setGames(data))
+  }, [])
+
   return (
     <View style={styles.container}>
-<Image source={LogoImg}
-style={styles.logo}
-/>
-<Heading title="Econtre o seu duo!" subtitle="Selecione o game que deseja jogar..."/> 
+      <Image source={LogoImg}
+        style={styles.logo}
+      />
+      <Heading title="Econtre o seu duo!" subtitle="Selecione o game que deseja jogar..." />
 
-<FlatList 
-contentContainerStyle={styles.contentList}
-data={GAMES}
-keyExtractor={item => item.id}
-renderItem={({item}) =>(
-  <GameCard 
-data={item}
- />
-  )}
-  showsHorizontalScrollIndicator={false}
-  horizontal
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={games}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GameCard
+            data={item}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
 
-/>
-   </View>
+      />
+    </View>
   );
 }
