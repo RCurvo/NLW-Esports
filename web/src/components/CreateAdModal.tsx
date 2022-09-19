627d0e43d50ce50de91d7c8b4c413db51ff84fd6
RCurvo <renan.curvo@gmail.com>
2022-09-19 13:17:36 -0300

    Makes modal into a component and create selector

---
+++ web/src/components/CreateAdModal.tsx
@@ -0,0 +1,127 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import { Check, GameController } from 'phosphor-react';
+import { Input } from './Form/input'
+import * as Checkbox from '@radix-ui/react-checkbox'
+import { useEffect, useState } from 'react';
+
+interface Game {
+    id: string;
+    title: string;
+}
+
+
+export function CreateAdModal() {
+    const [games, setGames] = useState<Game[]>([])
+
+    useEffect(() => {
+        fetch('http://localhost:3333/games')
+            .then(response => response.json())
+            .then(data => setGames(data))
+    }, [])
+    return (
+        <Dialog.Portal>
+            <Dialog.Overlay className='bg-black/60 inset-0 fixed'>
+                <Dialog.Content className='fixed bg-[#2a2634] py-8 text-white px-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
+                    <Dialog.Title className='text-3xl font-black'>
+                        Publique um anúncio
+                    </Dialog.Title>
+                    <form className='mt-8 flex flex-col gap-4' action="">
+                        <div className='flex flex-col gap-2'>
+                            <label htmlFor="game" className='font-semibold'>Qual o Game?</label>
+                            <select
+                                id="game"
+                                className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none'
+                            >
+                                <option disabled selected>Selecione o game que deseja jogar</option>
+                                {games.map(game => {
+                                    return (
+                                        <option key={game.id} value={game.id}>{game.title}</option>
+                                    )
+                                })}
+                            </select>
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                            <label htmlFor='name'> Seu nome (ou nickname)</label>
+                            <Input id="name" placeholder='Como te chamam dentro do game' />
+                        </div>
+                        <div className='grid grid-cols-2 gap-6'>
+                            <div className='flex flex-col gap-2'>
+                                <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
+                                <Input id="yearsPlaying" type='number' placeholder='Tudo bem ser ZERO' />
+                            </div>
+                            <div className='flex flex-col gap-2'>
+                                <label htmlFor="discord">Qual o seu Discord?</label>
+                                <Input id="discord" type='text' placeholder='Usuário#0000' />
+                            </div>
+                        </div>
+                        <div className='flex gap-6'>
+                            <div className='flex flex-col gap-2'>
+                                <label id='weekDays'>Quando costuma jogar?</label>
+                                <div className='flex flex-col'>
+                                    <div className='flex justify-center gap-1'>
+                                        <button
+                                            title='Domingo'
+                                            className='w-8 h-8 rounded bg-zinc-900'
+                                        >D</button>
+                                        <button
+                                            title='Segunda'
+                                            className='w-8 h-8 rounded bg-zinc-900'
+                                        >S</button>
+                                        <button
+                                            title='Terça'
+                                            className='w-8 h-8 rounded bg-zinc-900'
+                                        >T</button>
+                                        <button
+                                            title='Quarta'
+                                            className='w-8 h-8 rounded bg-zinc-900'
+                                        >Q</button>
+                                    </div>
+                                    <div className='flex justify-center gap-1 mt-2'>
+                                        <button
+                                            title='Quinta'
+                                            className='w-8 h-8 rounded bg-zinc-900'
+                                        >Q</button>
+                                        <button
+                                            title='Sexta'
+                                            className='w-8 h-8 rounded bg-zinc-900'
+                                        >S</button>
+                                        <button
+                                            title='Sábado'
+                                            className='w-8 h-8 rounded bg-zinc-900'
+                                        >S</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className='flex flex-col gap-2 flex-1'>
+                                <label htmlFor="hourStart">Qual horário do Dia?</label>
+                                <div className='grid grid-cols-2 gap-2'>
+                                    <Input id='hourStart' type="time" placeholder='De' />
+                                    <Input id='hourEnd' type="time" placeholder='Até' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='mt-2 flex items-center gap-2 text-sm'>
+                            <Checkbox.Root className='w-6 h-6 p-1 rounded bg-zinc-700'>
+                                <Checkbox.Indicator>
+                                    <Check className='w-4 h-4 text-emerald-400' />
+                                </Checkbox.Indicator>
+                            </Checkbox.Root>Costumo me conectar ao chat de voz
+                        </div>
+                        <footer className='mt-4 flex justify-end gap-4'>
+                            <Dialog.Close className='bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600'>
+                                Cancelar
+                            </Dialog.Close>
+                            <button type='submit'
+                                className='bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600'
+                            >
+                                <GameController size={24} /> Encontrar duo
+                            </button>
+                        </footer>
+                    </form>
+
+                </Dialog.Content>
+            </Dialog.Overlay>
+        </Dialog.Portal>
+    )
+}
